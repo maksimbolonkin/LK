87a62ec4c37dc061efe3b0e29db1a80dc9a5c28b
maksimbolonkin <maksimbolonkin@gmail.com>
2015-04-28 14:05:02 +0300

    changed transformation from J -> I to I->J (for purpose of comparison of affine matrices)

--- LK/LK/main.cpp
+++ LK/LK/main.cpp
@@ -12,10 +12,10 @@ int main(int argc, char *argv[])
 		exit(-1);
 	}
 
-	//Mat fixed = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-	//fixed.convertTo(fixed, CV_64FC1);
-	Mat moving = imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
-	moving.convertTo(moving, CV_64FC1);
+	Mat fixed = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+	fixed.convertTo(fixed, CV_64FC1);
+	//Mat moving = imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
+	//moving.convertTo(moving, CV_64FC1);
 	// read the images I J
 
 	//Point2f src[3]; src[0] = Point2f(50, 50); src[1] = Point2f(200, 50); src[2] = Point2f(50, 200); 
@@ -23,9 +23,9 @@ int main(int argc, char *argv[])
 	//Mat X = getAffineTransform(src, dst);
 	Mat X = getRotationMatrix2D(Point2f(190, 100), -35, 0.9);
 	cout<<X<<endl;
-	Mat fixed;
-	warpAffine(moving, fixed, X, moving.size());
-	imwrite("genfixed.jpeg", fixed);
+	Mat moving;
+	warpAffine(fixed, moving, X, fixed.size());
+	imwrite("genmoving.jpeg", moving);
 
 	cout<<"Images have been read..."<<endl;
 
