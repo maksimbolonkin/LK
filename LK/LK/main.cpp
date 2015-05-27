656017f83028d46d4b44bf4093180042dd9a0a63
maksimbolonkin <maksimbolonkin@gmail.com>
2015-05-27 10:17:22 +0300

    refactored code, included mask and square-diff calculation

--- LK/LK/main.cpp
+++ LK/LK/main.cpp
@@ -1,4 +1,4 @@
-
+#include "ImageRegistrationLK.h"
 #include "utils.hxx"
 #include <fstream>
 
@@ -8,17 +8,19 @@ int main(int argc, char *argv[])
 	ofstream fout("result.txt");
 
 
-	Mat bg = imread("background.jpeg", CV_LOAD_IMAGE_GRAYSCALE);
+	Mat bg = imread("bg5.jpeg", CV_LOAD_IMAGE_GRAYSCALE);
 	bg.convertTo(bg, CV_64FC1);
-	Mat pattern = imread("pattern.jpeg", CV_LOAD_IMAGE_GRAYSCALE);
+	Mat pattern = imread("pattern3.jpeg", CV_LOAD_IMAGE_GRAYSCALE);
 	pattern.convertTo(pattern, CV_64FC1);
+	Mat mask = formPatternMask(pattern);
+	imwrite("mask.jpeg", mask);
 
 	// resize pattern
 	Mat newpat;
 	resize(pattern, newpat, Size(0,0), 1.0, 1.0, INTER_LINEAR);
 
-	Mat fixed = formImage(bg, pattern, Point2d(100, 100));
-	Mat moving = formImage(bg, newpat, Point2d(105, 100));
+	Mat fixed = formImage(bg, pattern, mask, Point2d(100, 100));
+	Mat moving = formImage(bg, newpat, mask, Point2d(105, 95));
 
 	int wx = MAX(pattern.size().width, newpat.size().width), wy = MAX(pattern.size().height, newpat.size().height);
 
@@ -32,8 +34,10 @@ int main(int argc, char *argv[])
 	reg.setFixedImage(fixed);
 	reg.setMovingImage(moving);
 	reg.setNumberOfLevels(3);
+	reg.setMaxIterations(100);
 	reg.setWindowSize( Size(wx,wy));
 	reg.setWindowOffset(Point2d(100,100));
+	reg.setMask(mask);
 	reg.runRegistration();
 
 	Mat aff = reg.getTransform();
