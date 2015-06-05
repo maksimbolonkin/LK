24341a048faa62f80c103012667a03c0e46ad18b
maksimbolonkin <maksimbolonkin@gmail.com>
2015-06-05 14:59:36 +0300

    -

--- LK/LK/main.cpp
+++ LK/LK/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
 {
 	ofstream fout("result.txt");
 
+	//----- read bg and pattern, form image
 
 	Mat bg = imread("bg5.jpeg", CV_LOAD_IMAGE_GRAYSCALE);
 	bg.convertTo(bg, CV_64FC1);
@@ -14,28 +15,29 @@ int main(int argc, char *argv[])
 	pattern.convertTo(pattern, CV_64FC1);
 	Mat mask = formPatternMask(pattern);
 	imwrite("mask.jpeg", mask);
-
 	// resize pattern
 	Mat newpat;
-	//Mat X = getRotationMatrix2D(Point2f(pattern.size().width/2, pattern.size().height/2), 17, 0.85);
-	//warpAffine(pattern, newpat, X,Size(0,0));
+	////Mat X = getRotationMatrix2D(Point2f(pattern.size().width/2, pattern.size().height/2), 17, 0.85);
+	////warpAffine(pattern, newpat, X,Size(0,0));
 	resize(pattern, newpat, Size(0,0), 1.15, 1.15, INTER_LINEAR);
-	//pattern.copyTo(newpat);
-
+	////pattern.copyTo(newpat);
 	Mat fixed = formImage(bg, pattern, mask, Point2d(100, 100));
 	Mat moving = formImage(bg, newpat, formPatternMask(newpat), Point2d(97, 104));
+	imwrite("1.jpeg", fixed);
+	imwrite("2.jpeg", moving);
+	int wx = pattern.size().width, wy = pattern.size().height;
+	Point2d offset(100, 100);
+
+	//----- read from file
+
 	//Mat fixed = imread("fixed.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 	//fixed.convertTo(fixed, CV_64FC1);
 	//Mat moving = imread("moving.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 	//moving.convertTo(moving, CV_64FC1);
-
-	int wx = pattern.size().width, wy = pattern.size().height;
-	Point2d offset(100, 100);
 	//int wx = 284, wy = 76;
 	//Point2d offset(1438, 421);
 
-	imwrite("1.jpeg", fixed);
-	imwrite("2.jpeg", moving);
+	
 
 
 	//cout<<"Images have been read..."<<endl;
